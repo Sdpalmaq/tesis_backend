91d45eca3eb7079bd2d5b1b8b3e8e1b64d4965d4
Sdpalmaq <sdpalmaq@utn.edu.ec>
2025-01-17 22:22:23 -0500

    update

--- src/server.js
+++ src/server.js
@@ -20,11 +20,8 @@ wss.on("connection", (ws) => {
 });
 
 // Suscribirse al tópico MQTT para eventos de ESP32
-subscribeToTopic("sistema/+/notificaciones", (message, topic) => {
-  const topicParts = topic.split("/"); // Dividir el tópico en partes
-  const esp32Id = topicParts[1]; // Obtener el id_esp32 del segundo segmento
-
-  console.log(`Evento recibido desde ${esp32Id}:`, message);
+subscribeToTopic("sistema/+/notificaciones", (message, id_esp32) => {
+  console.log(`Evento recibido del dispositivo ${id_esp32}:`, message);
 
   // Retransmitir el evento a todos los clientes WebSocket conectados
   wss.clients.forEach((client) => {
