5728a2d3b32ced7ba9cccfce0644f4cf600fb3c7
Sdpalmaq <sdpalmaq@utn.edu.ec>
2025-02-04 15:10:11 -0500

    add new host db

--- src/config/database.js
+++ src/config/database.js
@@ -2,21 +2,25 @@ import pkg from 'pg';
 const { Pool } = pkg;
 import 'dotenv/config';
 
-// // conexion local con postgres
-// const pool = new Pool({
-//   host: "localhost",
-//   port: 5432,
-//   database: "tesis_v2",
-//   user: "postgres",
-//   password: "St@0402003602",
-// });
-// export default pool;
+// // onexion local con postgres
+/*
 const pool = new Pool({
-  host: "kesavan.db.elephantsql.com",
+host: "localhost",
+port: 5432,
+ database: "tesis_v2",
+ user: "postgres",
+ password: "St@0402003602",
+});*/
+//export default pool;
+
+
+const pool = new Pool({
+  host: "ep-square-term-a8b6ch3u-pooler.eastus2.azure.neon.tech",
   port: 5432,
-  database: "rvpkczjr",
-  user: "rvpkczjr",
-  password: "gItECzAW6G00yA6faJs18VQ2vgCB8YHv",
+  database: "neondb",
+  user: "neondb_owner",
+  password: "npg_TXElGZkCL6g8",
+  ssl: { rejectUnauthorized: false } // ⚠️ Esto activa SSL obligatorio
 });
 
 export default pool;
