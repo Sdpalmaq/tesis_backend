2be900029f2ed29551218e17e812f1a1d68fecbe
Sdpalmaq <sdpalmaq@utn.edu.ec>
2025-02-05 09:12:47 -0500

    change cors

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173", // URL del frontend
+    origin: "https://tesis-frontend-nine.vercel.app/" || "http://localhost:3000",
     credentials: true,
   })
 );
