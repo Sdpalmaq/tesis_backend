62f340fe4d472dd3c2c8c1ab1f78e3edd4629d49
Sdpalmaq <sdpalmaq@utn.edu.ec>
2025-06-12 12:26:39 -0500

    refactor: remove trailing slash from production CORS URL

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://tesis-frontend-nine.vercel.app/"
+    "https://tesis-frontend-nine.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
