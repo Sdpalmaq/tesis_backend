c59f53d87dc3f937557495c3f0d102418d8d4614
Sdpalmaq <sdpalmaq@utn.edu.ec>
2025-06-12 12:20:42 -0500

    refactor: update CORS origin URL to use the correct production link

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://tesis-frontend-dckmg9m51-sdpalmaqs-projects.vercel.app"
+    "https://tesis-frontend-nine.vercel.app/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { subscribeToTopic } from "./config/mqtt.config.js";
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://tesis-frontend-dckmg9m51-sdpalmaqs-projects.vercel.app"
+    "https://tesis-frontend-nine.vercel.app/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
